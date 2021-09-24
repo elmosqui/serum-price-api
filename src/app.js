a88ea630001648d94612d247a6fa3ff1d39fd338
elmosqui <88122276+elmosqui@users.noreply.github.com>
2021-09-24 06:02:31 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,13 +15,6 @@ const channels = require('./channels');
 
 const app = express(feathers());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
 
 app.configure(configuration());
 app.use(helmet({
@@ -31,6 +24,14 @@ app.use(cors());
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 // Set up Plugins and providers
 app.configure(express.rest());
