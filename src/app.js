9b69ee5453a85f0fdcec69bd5ac04cfaa7df5b44
elmosqui <88122276+elmosqui@users.noreply.github.com>
2021-09-24 05:59:26 +0200

    cors

--- src/app.js
+++ src/app.js
@@ -15,6 +15,14 @@ const channels = require('./channels');
 
 const app = express(feathers());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.configure(configuration());
 app.use(helmet({
   contentSecurityPolicy: false,
